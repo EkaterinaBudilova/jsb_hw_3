708a920242023c7f08d36c5fa33e0cd9a76ac79e
EkaterinaBudilova <ekaterinazh1@mail.ru>
2018-02-09 16:32:42 +1200

    дополнила 1 задачу

--- piano/script.js
+++ piano/script.js
@@ -1,81 +1,41 @@
-let mode = 'middle';
-//const sounds = document.getElementsByTagName('audio');
-//const set = document.getElementsByTagName('ul');
-
+const keys = document.getElementsByTagName('li');
+const sounds = document.getElementsByTagName('audio');
 const lower = ['sounds/lower/first.mp3', 'sounds/lower/second.mp3', 'sounds/lower/third.mp3', 'sounds/lower/fourth.mp3', 'sounds/lower/fifth.mp3'];
-
 const middle = ['sounds/middle/first.mp3', 'sounds/middle/second.mp3', 'sounds/middle/third.mp3', 'sounds/middle/fourth.mp3', 'sounds/middle/fifth.mp3'];
-
 const higher = ['sounds/higher/first.mp3', 'sounds/higher/second.mp3', 'sounds/higher/third.mp3', 'sounds/higher/fourth.mp3', 'sounds/higher/fifth.mp3'];
 
-//const sounds = document.getElementsByTagName('audio');
+const p = document.getElementsByTagName('ul');
+const piano = p[0];
 
-let mode = 'middle';
-//const sounds = document.getElementsByTagName('audio');
-//const set = document.getElementsByTagName('ul');
+document.addEventListener('keydown', func) 
 
+function func(event) {
+  console.log(event.code);
+  if ((event.code === 'AltLeft') || (event.code === 'AltRight')) {
+    piano.classList.remove('middle');
+    piano.classList.add('higher');
+  } else if ((event.code === 'ShiftLeft') || (event.code === 'ShifRight')) {
+    piano.classList.remove('middle');
+    piano.classList.add('lower');
+  }
+}
 
+for (let i = 0; i < sounds.length; i++) {
+  if (piano.classList.contains('middle')) {
+    sounds[i].src = middle[i];
+  } else if (piano.classList.contains('lower')) {
+    sounds[i].src = lower[i];
+  } else if (piano.classList.contains('higher')) {
+    sounds[i].src = higher[i];
+  }
+}
 
-const keys = document.getElementsByTagName('li');
-const play = function() {
-  const sounds = this.getElementsByTagName('audio');
-  const player = sounds[0];
-  player.src = middle[0];//сюда как-то нужно передать номер звука в массиве
+function play() {
+  let players = this.getElementsByTagName('audio');
+  let player = players[0];
   player.play();
 }
+
 for (let i = 0; i < keys.length; i++) {
-  
-  keys[i].addEventListener('click', play) ;
-  
+  keys[i].addEventListener('click', play);
 }
-
-
-
-//const keys = document.getElementsByTagName('li');
-
-//for (let j = 0; j < keys.length; j++) {
-//  sounds[j] = keys[j].getElementsByTagName('audio');
-//f
-//for (let i = 0; i < sounds.length; i++) {
-//  sounds[i].src = middle[i];
-//}
-//
-//
-//
-//const play = function () {
-//  this.play();
-//}
-//document.addEventListener('click', play);
-
-//function func = function(event) {
-//  if (event.shiftKey) {
-//    mode = 'lower';
-//    set[0].classList.add('lower');
-//    set[0].classList.remove('higher');
-//  } else if (event.altlKey) {
-//    mode = 'higher';
-//  } 
-//}
-//
-//
-//  sounds[0].onclick = () => {
-//    sounds[0].play();
-//  
-//}
-//document.addEventListener('keydown', func);
-//if (set[0].classList.contains('lower')) {
-//  for (let i = 0; i < sounds.length; i++) {
-//    sounds[i].src = lower[i];
-//  }  
-//} else if (set[0].classList.contains('middle')) {
-//  for (let i = 0; i < sounds.length; i++) {
-//    sounds[i].src = middle[i];
-//  }  
-//} else if (set[0].classList.contains('higher')) {
-//  for (let i = 0; i < sounds.length; i++) {
-//    sounds[i].src = higher[i];
-//  }  
-//}
-//
-//
-//document.addEventListener('click', play);
