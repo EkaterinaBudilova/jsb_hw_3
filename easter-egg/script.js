ad119ededed66acfc5c6f090da87ba9359645621
EkaterinaBudilova <ekaterinazh1@mail.ru>
2018-02-10 10:54:24 +1200

    задача 2 v.2

--- easter-egg/script.js
+++ easter-egg/script.js
@@ -1,13 +1,29 @@
 const navigation = document.getElementsByTagName('nav');
+let str = [];
+const egg = ['KeyY', 'KeyT', 'KeyN', 'KeyJ','KeyK', 'KeyJ','KeyU', 'KeyB', 'KeyZ'];
+let symbol;
 function show(event) {
   if ((event.ctrlKey) && (event.altKey)) {
     console.log('true');
-    if (event.code === 'KeyP') {
+    if (event.code === 'KeyT') {
       navigation[0].classList.toggle('visible');
       console.log('event');
     }
-  } 
+  } else {
+    symbol = event.code;
+    str.push(symbol);
+    console.log(str);
+  }
 }
 
-document.addEventListener('keyup', show());
+document.addEventListener('keyup', show);
 
+while(str.length <= egg.length) {
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== egg[i]) {
+      str = [];
+    } else {
+      console.log('yyy');
+    }
+  }
+}
