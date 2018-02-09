522f08a0a07b5a041b67dfcd347115a581715931
EkaterinaBudilova <ekaterinazh1@mail.ru>
2018-02-09 14:25:44 +1200

    доделала 2 задачу

--- skateboard-gallery/script.js
+++ skateboard-gallery/script.js
@@ -1,7 +1,18 @@
 const items = document.getElementsByTagName('a');
+const largePic = document.getElementById('view');
 
 function select(event) {
+  let pic = this.href;
+  console.log(pic);
+
   event.preventDefault();
+  for (let i = 0; i< items.length; i++) {
+    items[i].classList.remove('gallery-current');
+  }
   this.classList.add('gallery-current');
+  largePic.src = pic;
+  
 }
-items.addEventListener('click', select);
+for (let i = 0; i < items.length; i++) {
+  items[i].addEventListener('click', select);
+}
