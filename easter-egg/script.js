bcf1d76869e1c338fad977851a9daa2d6c0ee663
EkaterinaBudilova <ekaterinazh1@mail.ru>
2018-03-30 11:57:46 +1200

    easter-egg v.2

--- easter-egg/script.js
+++ easter-egg/script.js
@@ -1,29 +1,40 @@
 const navigation = document.getElementsByTagName('nav');
 let str = [];
 const egg = ['KeyY', 'KeyT', 'KeyN', 'KeyJ','KeyK', 'KeyJ','KeyU', 'KeyB', 'KeyZ'];
+const secret = document.querySelector('div.secret');
 let symbol;
-function show(event) {
-  if ((event.ctrlKey) && (event.altKey)) {
-    console.log('true');
-    if (event.code === 'KeyT') {
-      navigation[0].classList.toggle('visible');
-      console.log('event');
+
+function check() {
+  let eq = false;
+  for (let i = 0; i < egg.length; i++) {
+    if (str[i] !== egg[i]) {
+      str = [];
+      eq = false;
+      break;
+    } else {
+      eq = true;
     }
+  }
+  if (eq) {
+    secret.classList.add('visible');
+  }
+}
+
+function show(event) {
+  if ((event.ctrlKey) && (event.altKey) && (event.code === 'KeyT') ) {
+    navigation[0].classList.toggle('visible');
   } else {
     symbol = event.code;
-    str.push(symbol);
-    console.log(str);
+    if (str.length < egg.length) {
+      str.push(symbol);
+    } else {
+      str = [];
+      str.push(symbol);
+    }
+  }
+  if (str.length == egg.length) {
+    check();
   }
 }
 
 document.addEventListener('keyup', show);
-
-while(str.length <= egg.length) {
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] !== egg[i]) {
-      str = [];
-    } else {
-      console.log('yyy');
-    }
-  }
-}
